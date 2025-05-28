f7fdb39bf77ec2c48e6c29b3f7912ae183f74167
Wail Bouymaj <wailbouymaj@gmail.com>
2025-05-28 22:31:29 +0100

    feat: add UserContext for managing user authentication state with Google Sign-In

---
+++ contexts/UserContext.tsx
@@ -0,0 +1,97 @@
+import {
+    getAuth,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    signInWithCredential,
+} from '@react-native-firebase/auth';
+import {
+    GoogleSignin,
+    isErrorWithCode,
+    statusCodes,
+} from '@react-native-google-signin/google-signin';
+import React, { createContext, ReactNode, useEffect, useState } from 'react';
+
+export type User = {
+    id?: string;
+    name?: string | null;
+    email: string | null;
+    photo?: string | null;
+    familyName?: string | null;
+    givenName?: string | null;
+};
+
+type UserContextType = {
+    user: User | null;
+    login: () => Promise<void | string>;
+    logout: () => Promise<void>;
+    initializing: boolean;
+};
+
+export const UserContext = createContext<UserContextType | undefined>(
+    undefined
+);
+
+export function UserProvider({ children }: { children: ReactNode }) {
+    const [user, setUser] = useState<User | null>(null);
+    const [initializing, setInitializing] = useState(true);
+
+    function handleAuthStateChanged(user: User | null) {
+        setUser(user);
+        if (initializing) setInitializing(false);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(
+            getAuth(),
+            handleAuthStateChanged
+        );
+
+        return unsubscribe;
+    }, [initializing]);
+
+    const login = async () => {
+        try {
+            await GoogleSignin.hasPlayServices({
+                showPlayServicesUpdateDialog: true,
+            });
+
+            const signInResult = await GoogleSignin.signIn();
+            console.log('signInResult', signInResult.data?.user);
+
+            const googleCredential = GoogleAuthProvider.credential(
+                signInResult.data?.idToken
+            );
+            await signInWithCredential(getAuth(), googleCredential);
+        } catch (error) {
+            console.error('Google Sign-In Error in context:', error);
+            if (isErrorWithCode(error)) {
+                switch (error.code) {
+                    case statusCodes.SIGN_IN_CANCELLED:
+                        return 'Sign In Cancelled';
+                    case statusCodes.IN_PROGRESS:
+                        return 'Sign In is already in progress';
+                    case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+                        return 'Play Services Not Available or Outdated';
+                    default:
+                        return 'Something went wrong';
+                }
+            }
+            return 'Unknown error occurred';
+        }
+    };
+
+    const logout = async () => {
+        try {
+            await GoogleSignin.signOut();
+            await getAuth().signOut();
+        } catch (error) {
+            console.error('Logout Error:', error);
+        }
+    };
+
+    return (
+        <UserContext.Provider value={{ user, login, logout, initializing }}>
+            {children}
+        </UserContext.Provider>
+    );
+}
