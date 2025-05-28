4674694b5b7e0d168505510e4806cb10a5e5eb05
Wail Bouymaj <wailbouymaj@gmail.com>
2025-05-28 17:36:28 +0100

    refactor: removed login logic from welcome screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,76 +1,36 @@
-import auth from '@react-native-firebase/auth';
+import { Spacer, ThemedText, ThemedView } from '@/components';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
-import { useEffect, useState } from 'react';
-import {
-    Button,
-    KeyboardAvoidingView,
-    StyleSheet,
-    TextInput,
-    View,
-} from 'react-native';
+import { Link } from 'expo-router';
+import { useEffect } from 'react';
+import { StyleSheet } from 'react-native';
 
 export default function Index() {
-    const [user, setUser] = useState({ email: '', password: '' });
-    const [loading, setLoading] = useState(false);
-
     useEffect(() => {
         GoogleSignin.configure({
-            iosClientId: "1096567925493-nrsfralu5tdqcg2j480g2hh870i4bjdg.apps.googleusercontent.com",
-            webClientId: "1096567925493-ofl8psaj4pvbhvg1e4cv8a7sqbehkl22.apps.googleusercontent.com",
+            iosClientId:
+                '1096567925493-nrsfralu5tdqcg2j480g2hh870i4bjdg.apps.googleusercontent.com',
+            webClientId:
+                '1096567925493-ofl8psaj4pvbhvg1e4cv8a7sqbehkl22.apps.googleusercontent.com',
             profileImageSize: 150,
-        })
-    })
-
-    const signUp = async () => {
-        setLoading(true);
-        try {
-            await auth().createUserWithEmailAndPassword(
-                user.email,
-                user.password
-            );
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    const signIn = () => {};
+        });
+    });
 
     return (
-        <View style={styles.container}>
-            <KeyboardAvoidingView behavior='padding'>
-                <TextInput
-                    style={styles.input}
-                    placeholder='Email'
-                    value={user.email}
-                    onChangeText={(email) => setUser({ ...user, email })}
-                    keyboardType='email-address'
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder='Password'
-                    value={user.password}
-                    onChangeText={(password) => setUser({ ...user, password })}
-                    secureTextEntry
-                />
-                <Button title='Sign Up' onPress={signUp} />
-                <Button title='Login' onPress={signIn} />
-            </KeyboardAvoidingView>
-        </View>
+        <ThemedView style={styles.container}>
+            <ThemedText type='title'>Welcome to my App</ThemedText>
+            <Spacer height={2} />
+            <Link href='/login'>
+                <ThemedText type='link'>Go to Login Page</ThemedText>
+            </Link>
+        </ThemedView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        marginHorizontal: 20,
+        padding: 20,
         flex: 1,
         justifyContent: 'center',
-    },
-    input: {
-        marginVertical: 4,
-        height: 50,
-        borderWidth: 1,
-        borderRadius: 4,
-        padding: 10,
-        backgroundColor: '#fff',
+        alignItems: 'center',
     },
 });
