e55a46d283aba5969111e576a4b12d5537da6dbb
Wail Bouymaj <wailbouymaj@gmail.com>
2025-05-28 11:56:11 +0100

    feat: add user authentication with Firebase in index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,67 @@
-import { Text, View } from "react-native";
+import auth from '@react-native-firebase/auth';
+import { useState } from 'react';
+import {
+    Button,
+    KeyboardAvoidingView,
+    StyleSheet,
+    TextInput,
+    View,
+} from 'react-native';
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    const [user, setUser] = useState({ email: '', password: '' });
+    const [loading, setLoading] = useState(false);
+
+    const signUp = async () => {
+        setLoading(true);
+        try {
+            await auth().createUserWithEmailAndPassword(
+                user.email,
+                user.password
+            );
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const signIn = () => {};
+
+    return (
+        <View style={styles.container}>
+            <KeyboardAvoidingView behavior='padding'>
+                <TextInput
+                    style={styles.input}
+                    placeholder='Email'
+                    value={user.email}
+                    onChangeText={(email) => setUser({ ...user, email })}
+                    keyboardType='email-address'
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='Password'
+                    value={user.password}
+                    onChangeText={(password) => setUser({ ...user, password })}
+                    secureTextEntry
+                />
+                <Button title='Sign Up' onPress={signUp} />
+                <Button title='Login' onPress={signIn} />
+            </KeyboardAvoidingView>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginHorizontal: 20,
+        flex: 1,
+        justifyContent: 'center',
+    },
+    input: {
+        marginVertical: 4,
+        height: 50,
+        borderWidth: 1,
+        borderRadius: 4,
+        padding: 10,
+        backgroundColor: '#fff',
+    },
+});
