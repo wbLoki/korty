a660ae7b68539d5dd35723fe771861cc108098c8
Wail Bouymaj <wailbouymaj@gmail.com>
2025-05-30 00:18:38 +0100

    feat(i18n): integrate i18n for localized text in Home component

--- app/(dashboard)/home.tsx
+++ app/(dashboard)/home.tsx
@@ -1,10 +1,11 @@
 import { Spacer, ThemedText, ThemedView } from '@/components';
+import { t } from '@/i18n/i18n';
 import { StyleSheet } from 'react-native';
 
 export default function Home() {
     return (
         <ThemedView safe style={styles.container}>
-            <ThemedText type='title'>This is Home</ThemedText>
+            <ThemedText type='title'>{t('hello')}</ThemedText>
             <Spacer height={2} />
         </ThemedView>
     );
