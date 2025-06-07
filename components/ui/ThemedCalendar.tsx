4a3aa2cecbdd6a4bc1a9550e540ce67f745a01b2
wbLoki <wailbouymaj@gmail.com>
2025-06-07 03:30:57 +0100

    styles: update colors to use variable

--- components/ui/ThemedCalendar.tsx
+++ components/ui/ThemedCalendar.tsx
@@ -1,36 +1,30 @@
 import { useThemeColor } from '@/hooks/useThemeColor';
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import CalendarStrip from 'react-native-calendar-strip';
 
 const ThemedCalendar = () => {
     const backgroundColor = useThemeColor('surface');
     const color = useThemeColor('textPrimary');
+    const gray = useThemeColor('textSecondary');
+    const blue = useThemeColor('primary');
     return (
         <CalendarStrip
-                scrollable
-                rightSelector={[]}
-                leftSelector={[]}
-                style={{ height: 60, paddingTop: 10, paddingBottom: 10 }}
-                calendarColor={backgroundColor}
-                calendarHeaderStyle={{ display: 'none' }}
-                dateNumberStyle={{ color }}
-                dateNameStyle={{ color: 'gray' }}
-                highlightDateNumberStyle={{ color: 'white' }}
-                highlightDateNameStyle={{ color: 'white' }}
-                highlightDateContainerStyle={{ backgroundColor: 'dodgerblue' }}
-                selectedDate={new Date()}
-                onDateSelected={(date) => console.log(date)}
-                />
+            scrollable
+            rightSelector={[]}
+            leftSelector={[]}
+            style={{ height: 60, paddingTop: 10, paddingBottom: 10 }}
+            calendarColor={backgroundColor}
+            calendarHeaderStyle={{ display: 'none' }}
+            dateNumberStyle={{ color }}
+            dateNameStyle={{ color: gray }}
+            highlightDateNumberStyle={{ color }}
+            highlightDateNameStyle={{ color }}
+            highlightDateContainerStyle={{ backgroundColor: blue }}
+            selectedDate={new Date()}
+            onDateSelected={(date) => console.log(date)}
+        />
     );
 };
 
 export default ThemedCalendar;
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-});
