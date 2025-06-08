cc17e8c66abfff2babb1e35e968afdde34240331
Wail Bouymaj <wailbouymaj@gmail.com>
2025-06-08 13:23:22 +0100

    style(layout): apply consistent styles for header based on theme

--- app/(dashboard)/messages/_layout.tsx
+++ app/(dashboard)/messages/_layout.tsx
@@ -1,6 +1,17 @@
+import { useThemeColor } from '@/hooks/useThemeColor';
 import { Stack } from 'expo-router';
 
 export default function AuthLayout() {
-    return <Stack screenOptions={{ animation: 'none', title: 'Messages' }} />;
+    const backgroundColor = useThemeColor('background');
+    const color = useThemeColor('textPrimary');
+    return (
+        <Stack
+            screenOptions={{
+                animation: 'none',
+                title: 'Messages',
+                headerStyle: { backgroundColor },
+                headerTitleStyle: { color },
+            }}
+        />
+    );
 }
-
