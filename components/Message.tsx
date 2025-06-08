d4c828b7fa566e9f5ed964bb5569518f6a8827d5
Wail Bouymaj <wailbouymaj@gmail.com>
2025-06-08 13:59:28 +0100

    refactor(Message): update props to use avatar and lastMessage

--- components/Message.tsx
+++ components/Message.tsx
@@ -9,30 +9,67 @@ import ThemedText from './ui/ThemedText';
 const Message = (props: MessageProps) => {
     const imagebg = useThemeColor('background');
     const backgroundColor = useThemeColor('surface');
-    const { name, image, date, message, count } = props;
+    const color = useThemeColor('textSecondary');
+    const { name, avatar, date, lastMessage, unread } = props;
     const timeAgo = getRelativeTime(new Date(date));
 
     return (
         <View style={[styles.container, { backgroundColor }]}>
-            <View>
+            <View
+                style={{ flexShrink: 0, aspectRatio: 1, paddingVertical: 12 }}
+            >
                 <Image
                     style={[{ backgroundColor: imagebg }, styles.image]}
-                    source={image}
+                    source={avatar}
                     placeholder={'user image'}
                     contentFit='contain'
                 />
             </View>
-            <View>
-                <View style={{ flexDirection: 'row', gap: 8 }}>
-                    <ThemedText>{name}</ThemedText>
-                    <ThemedText>{timeAgo}</ThemedText>
+            <View
+                style={{
+                    flexShrink: 1,
+                    gap: 4,
+                }}
+            >
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        gap: 8,
+                        justifyContent: 'space-between',
+                        width: '100%',
+                    }}
+                >
+                    <ThemedText type='defaultSemiBold'>{name}</ThemedText>
+                    <ThemedText
+                        style={{
+                            color,
+                            justifyContent: 'flex-end',
+                            fontSize: 12,
+                        }}
+                    >
+                        {timeAgo}
+                    </ThemedText>
                 </View>
-                <View style={{ flexDirection: 'row', gap: 8 }}>
-                    <ThemedText numberOfLines={1} style={{ flexShrink: 1 }}>
-                        {message}
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        gap: 16,
+                        justifyContent: 'space-between',
+                        width: '100%',
+                    }}
+                >
+                    <ThemedText
+                        numberOfLines={2}
+                        style={{ flexShrink: 1, fontSize: 15, color }}
+                    >
+                        {lastMessage}
                     </ThemedText>
                     <View>
-                        <ThemedText>{count}</ThemedText>
+                        {unread > 0 && (
+                            <View style={styles.notification}>
+                                <ThemedText>{unread}</ThemedText>
+                            </View>
+                        )}
                     </View>
                 </View>
             </View>
@@ -44,18 +81,26 @@ export default Message;
 
 const styles = StyleSheet.create({
     container: {
-        padding: 12,
+        flex: 1,
+        paddingHorizontal: 12,
         flexDirection: 'row',
-        gap: 12,
         borderBottomWidth: 1,
+        maxWidth: '100%',
+        height: 80,
     },
     image: {
-        // width: 40,
         aspectRatio: 1,
         flex: 1,
         height: '100%',
         borderRadius: '100%',
-        // margin: 16,
+    },
+    notification: {
+        backgroundColor: 'red',
+        width: 24,
+        height: 24,
+        borderRadius: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'white',
     },
 });
-
