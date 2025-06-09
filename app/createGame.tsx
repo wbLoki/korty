910f1c1bad0d1276327a0542f86da14b7c933429
Wail Bouymaj <wailbouymaj@gmail.com>
2025-06-09 17:15:55 +0100

    feat(createGame): add initial component for creating games

---
+++ app/createGame.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const createGame = () => {
+    return (
+        <View>
+            <Text>createGame</Text>
+        </View>
+    );
+};
+
+export default createGame;
+
+const styles = StyleSheet.create({});
