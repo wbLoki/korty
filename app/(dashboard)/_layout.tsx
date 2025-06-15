afca58fd9c32a32c9931e55e2f9535a275a69002
Wail Bouymaj <wailbouymaj@gmail.com>
2025-06-15 01:58:01 +0100

    feat(dashboard): implement custom NavBar for tab navigation

--- app/(dashboard)/_layout.tsx
+++ app/(dashboard)/_layout.tsx
@@ -1,69 +1,21 @@
 import UserOnly from '@/components/auth/UserOnly';
-import { useThemeColor } from '@/hooks/useThemeColor';
-import { Ionicons } from '@expo/vector-icons';
+import NavBar from '@/components/ui/NavBar';
 import { Tabs } from 'expo-router';
 
 const DashboardLayout = () => {
-    const active = useThemeColor('tabBarActive');
-    const inActive = useThemeColor('tabBarInactive');
     return (
         <UserOnly>
             <Tabs
-                screenOptions={{
-                    headerShown: false,
-                    tabBarStyle: {
-                        backgroundColor: useThemeColor('navBar'),
-                        paddingTop: 10,
-                    },
-                    tabBarActiveTintColor: active,
-                    tabBarInactiveTintColor: inActive,
-                    tabBarLabelPosition: 'beside-icon',
-                    tabBarItemStyle: { borderRadius: 20 },
-                }}
+                screenOptions={{ headerShown: false }}
+                tabBar={(props) => <NavBar {...props} />}
             >
-                <Tabs.Screen
-                    name='home'
-                    options={{
-                        title: 'Home',
-                        tabBarIcon: ({ focused }) => (
-                            <Ionicons
-                                name={focused ? 'home' : 'home-outline'}
-                                size={24}
-                                color={focused ? active : inActive}
-                            />
-                        ),
-                    }}
-                />
-                <Tabs.Screen
-                    name='search'
-                    options={{
-                        title: 'Search',
-                        tabBarIcon: ({ focused }) => (
-                            <Ionicons
-                                name='search'
-                                size={24}
-                                color={focused ? active : inActive}
-                            />
-                        ),
-                    }}
-                />
-                <Tabs.Screen
-                    name='profile'
-                    options={{
-                        title: 'Profile',
-                        tabBarIcon: ({ focused }) => (
-                            <Ionicons
-                                name={focused ? 'person' : 'person-outline'}
-                                size={24}
-                                color={focused ? active : inActive}
-                            />
-                        ),
-                    }}
-                />
+                <Tabs.Screen name='home' />
+                <Tabs.Screen name='search' />
+                <Tabs.Screen name='profile' />
+                <Tabs.Screen name='test' />
             </Tabs>
         </UserOnly>
     );
 };
 
 export default DashboardLayout;
-
